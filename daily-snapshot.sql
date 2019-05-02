76eada7fe8d67fc95cbee48280ee8026fd402912
Andrew Michael Spence Boyd <aboyd@its-boyd-mbp.bastropisdtx.org>
2019-05-02 14:25:37 -0500

    joined CATEGORY table for including category and category option fields

--- daily-snapshot.sql
+++ daily-snapshot.sql
@@ -1,7 +1,10 @@
+/* SQL query for returning records matching all tickets created on the current date */
+
 SELECT
     t.id as 'ID',
     t.assignedto as 'Technician',
     t.subject as 'Issue',
+    c.name as 'Category',
     DATE_FORMAT(t.createddate, "%Y-%m-%d") as 'Opened On:',
     s.name as 'Status'
 
@@ -9,6 +12,9 @@ SELECT
 
         JOIN status s ON
             s.id = t.statusid
+
+        JOIN category c ON
+            c.id = t.categoryid
 
     
     -- Return only tickets created today
