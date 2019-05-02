d1c5aedefe7f5f2ce229a99737bf8a2ca4291de1
Andrew Michael Spence Boyd <aboyd@its-boyd-mbp.bastropisdtx.org>
2019-05-02 14:37:04 -0500

    joined groups table and removed status column from results set

--- daily-snapshot.sql
+++ daily-snapshot.sql
@@ -2,11 +2,11 @@
 
 SELECT
     t.id as 'ID',
+    g.name as 'Campus',
     t.assignedto as 'Technician',
     t.subject as 'Issue',
     c.name as 'Category',
-    DATE_FORMAT(t.createddate, "%Y-%m-%d") as 'Opened On:',
-    s.name as 'Status'
+    DATE_FORMAT(t.createddate, "%Y-%m-%d") as 'Opened On:'
 
     FROM ticket t
 
@@ -16,6 +16,8 @@ SELECT
         JOIN category c ON
             c.id = t.categoryid
 
+        JOIN groups g ON
+            g.id = t.groupid
     
     -- Return only tickets created today
     WHERE DATE(t.createddate) = CURDATE()
